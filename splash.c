bfdc49bbec146744698443c055c466fbf04e41bb
tikhonov <tikhonov@haulmont.com>
2013-03-20 17:26:09 +0400

    Add info on build dependencies.

--- splash.c
+++ splash.c
@@ -18,6 +18,8 @@ with Splash. If not, see <http://www.gnu.org/licenses/>
 
 Various functions used in Splash are derived from fbi (Gerd Hoffmann <gerd@kraxel.org>)
 
+sudo apt-get install gcc uthash-dev
+
 gcc splash.c -lm -o splash /usr/lib/arm-linux-gnueabihf/libjpeg.a /usr/lib/arm-linux-gnueabihf/libfreetype.a /usr/lib/arm-linux-gnueabihf/libz.a -I/usr/include/freetype2/
 */
 
